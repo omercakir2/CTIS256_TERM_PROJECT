c19d9b6c5e4fd8b1a12136000d671f21bac7e744
okaan06 <kaan68111b@gmail.com>
2026-05-02 20:00:15 +0300

    feat: login and register working w/o session

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import bcrypt from 'bcryptjs';
+import db from './db.js';
 
 const app = express();
 
 app.use(express.static("public"))
 app.set("view engine","ejs")
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.APP_PORT || 3000;
 
@@ -12,10 +15,91 @@ app.get('/',(req,res)=>{
     res.render("index")
 })
 app.get('/login',(req,res)=>{
-    res.render('login')
+    res.render('login', { error: null, formData: {} });
 })
-app.get('/register',(req,res)=>{
-    res.render('register')
+
+
+app.post('/login', async (req,res)=>{
+
+
+const email = req.body.email.trim();
+const password = req.body.password.trim();
+
+    const [rows] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
+
+    if (rows.length > 0) {
+        const user = rows[0];
+        console.log("DBden Gelen Hash:", user.password + " - Email: " + user.email);
+        const match = await bcrypt.compare(password, user.password);
+        console.log("Bcrypt: ", match);
+        if (match) {
+            if(user.role === 'consumer') {
+
+                res.redirect("/main");
+            }
+            else {
+                res.redirect("/market/dashboard");
+            }
+        } else {
+            res.render('login', { error: "Invalid username or password", formData: req.body });
+            console.log("Invalid username or password");
+        }
+    //     if (password === 'password123' || password === user.password) {
+    //     console.log("yess");
+    //     return res.redirect("/market/dashboard");
+    // } else {
+    //     console.log("noo");
+    //     return res.redirect("/");
+    // }
+
+
+
+    } else {
+        console.log("user not found");
+        res.render('login', { error: "Invalid username or password", formData: req.body });
+    }
+
+
+
+
+})
+
+app.get('/register', (req, res) => {
+    res.render('register', { error: null, formData: {} });
+});
+
+app.post('/register', async (req,res)=>{
+    try {
+        const { email, password, confirmPassword, role, name, city, district } = req.body;
+
+        
+        if (password !== confirmPassword) {
+            console.log("Passwords do not match");
+            return res.render('register', { 
+            error: "passwords dont match", 
+            formData: req.body 
+        });
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password.trim(), saltRounds);
+
+        await db.query("INSERT INTO users (email, password, role, name, city, district, is_verified) VALUES (?, ?, ?, ?, ?, ?, 1)", [email.trim(), hashedPassword, role, name, city, district]);
+        console.log("Kayıt Başarılı: ", email);
+        res.redirect('/login');
+
+    } catch (error) {
+        console.error("Register Hatası:", error);
+        
+        if (error.code === 'ER_DUP_ENTRY') {
+            return res.send('<script>alert("Bu email zaten kayıtlı!"); window.location="/register";</script>');
+        }
+        res.status(500).send("Sunucu hatası oluştu.");
+    }
+
+
+
+
 })
 app.get('/main', (req, res) => {
     res.render('consumerPage');
