c0af938c2a6ebadf17521ed3a50c608354b060c8
okaan06 <kaan68111b@gmail.com>
2026-05-03 01:24:43 +0300

    style : format code

--- server.js
+++ server.js
@@ -28,37 +28,35 @@ const loginValidation = [
 ];
 
 app.use(express.static("public"))
-app.set("view engine","ejs")
+app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.APP_PORT || 3000;
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render("index")
 })
-app.get('/login',(req,res)=>{
+app.get('/login', (req, res) => {
     res.render('login', { error: null, formData: {} });
 })
 
 
-app.post('/login', loginValidation, async (req,res)=>{
+app.post('/login', loginValidation, async (req, res) => {
 
-const errors = validationResult(req);
-if (!errors.isEmpty()) {
-    return res.render('login', { error: errors.array()[0].msg, formData: req.body });
-}
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.render('login', { error: errors.array()[0].msg, formData: req.body });
+    }
 
 
-const email = req.body.email.trim();
-const password = req.body.password.trim();
+    const email = req.body.email.trim();
+    const password = req.body.password.trim();
 
     const [rows] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
 
     if (rows.length > 0) {
         const user = rows[0];
-        console.log("DBden Gelen Hash:", user.password + " - Email: " + user.email);
         const match = await bcrypt.compare(password, user.password);
-        console.log("Bcrypt: ", match);
         if (match) {
 
             req.session.user = {
@@ -67,7 +65,7 @@ const password = req.body.password.trim();
                 email: user.email,
                 role: user.role
             };
-            if(user.role === 'consumer') {
+            if (user.role === 'consumer') {
                 res.redirect("/main");
             }
             else {
@@ -75,20 +73,18 @@ const password = req.body.password.trim();
             }
         } else {
             res.render('login', { error: "Invalid username or password", formData: req.body });
-            console.log("Invalid username or password");
         }
-    //     if (password === 'password123' || password === user.password) {
-    //     console.log("yess");
-    //     return res.redirect("/market/dashboard");
-    // } else {
-    //     console.log("noo");
-    //     return res.redirect("/");
-    // }
+        //     if (password === 'password123' || password === user.password) {
+        //     console.log("yess");
+        //     return res.redirect("/market/dashboard");
+        // } else {
+        //     console.log("noo");
+        //     return res.redirect("/");
+        // }
 
 
 
     } else {
-        console.log("user not found");
         res.render('login', { error: "Invalid username or password", formData: req.body });
     }
 
@@ -110,32 +106,30 @@ app.post('/register', registerValidation, async (req, res) => {
     try {
         const { email, password, confirmPassword, role, name, city, district } = req.body;
 
-        
+
         if (password !== confirmPassword) {
-            console.log("Passwords do not match");
-            return res.render('register', { 
-            error: "passwords dont match", 
-            formData: req.body 
-        });
+            return res.render('register', {
+                error: "passwords dont match",
+                formData: req.body
+            });
         }
 
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password.trim(), saltRounds);
 
         await db.query("INSERT INTO users (email, password, role, name, city, district, is_verified) VALUES (?, ?, ?, ?, ?, ?, 1)", [email.trim(), hashedPassword, role, name, city, district]);
-        console.log("Kayıt Başarılı: ", email);
         res.redirect('/login');
 
     } catch (error) {
-        console.error("Register Hatası:", error);
-        
+
+
         if (error.code === 'ER_DUP_ENTRY') {
-            return res.render('register', { 
-            error: "This email is already registered!", 
-            formData: req.body 
-        });
+            return res.render('register', {
+                error: "This email is already registered!",
+                formData: req.body
+            });
         }
-        res.status(500).send("Sunucu hatası oluştu.");
+        res.status(500).send("An error occurred during registration. Please try again later.");
     }
 
 
@@ -145,16 +139,16 @@ app.post('/register', registerValidation, async (req, res) => {
 app.get('/main', (req, res) => {
     res.render('consumerPage');
 });
-app.get('/market/dashboard',(req,res)=>{
+app.get('/market/dashboard', (req, res) => {
     res.render('dashboard')
 })
-app.get('/market/addproduct',(req,res)=>{
+app.get('/market/addproduct', (req, res) => {
     res.render('addproduct')
 })
-app.get('/market/product/edit',(req,res)=>{
+app.get('/market/product/edit', (req, res) => {
     res.render('editproduct')
 })
-app.get('/cart',(req,res)=>{
+app.get('/cart', (req, res) => {
     res.render('cart')
 })
 
